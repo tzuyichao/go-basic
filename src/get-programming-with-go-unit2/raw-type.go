606ddf69b15177c376baa4fcf98805b623935370
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-17 16:45:06 +0800

    more

--- src/get-programming-with-go-unit2/raw-type.go
+++ src/get-programming-with-go-unit2/raw-type.go
@@ -4,5 +4,11 @@ import "fmt"
 
 func main() {
     fmt.Printf("%v is a %[1]T\n", "literal string")
-    fmt.Printf("%v is a %[1]T\n", `raw string literal`)
+	fmt.Printf("%v is a %[1]T\n", `raw string literal`)
+	
+	fmt.Println("peace be upon you\nupon you be peace")
+	fmt.Println(`strings can span multiple lines with the \n escape sequence`)
+	fmt.Println(`
+	peace be upon you
+	upon you be peace`)
 }
