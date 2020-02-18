2bed7e14574c271a7ad9f2164497ead2bec244bd
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-18 15:20:56 +0800

    Sprintf() and Atoi()

--- src/get-programming-with-go-unit2/itoa.go
+++ src/get-programming-with-go-unit2/itoa.go
@@ -8,5 +8,14 @@ import (
 func main() {
     countdown := 10
     str := "Lauch in T minus " + strconv.Itoa(countdown) + " seconds."
-    fmt.Println(str)
+	fmt.Println(str)
+	
+	str2 := fmt.Sprintf("Lauch in T minus %v seconds.", countdown)
+	fmt.Println(str2)
+	
+	size, err := strconv.Atoi("10a")
+	if err != nil {
+		fmt.Println("ascii to integer failed")
+	}
+	fmt.Println(size)
 }
