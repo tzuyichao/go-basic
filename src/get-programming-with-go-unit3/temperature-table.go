c0b71a6cb7dc382de2989e076dbe002e916f893b
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-19 16:56:48 +0800

    part1

--- src/get-programming-with-go-unit3/temperature-table.go
+++ src/get-programming-with-go-unit3/temperature-table.go
@@ -4,8 +4,45 @@ import (
     "fmt"
 )
 
+type celsius float64
+
+type fahrenheit float64
+
+type header func()
+
+type footer func()
+
+func (c celsius) fahrenheit() fahrenheit {
+	return fahrenheit((c*9.0/5.0)+32.0)
+}
+
+type body func()
+
+func drawTable(h header, b body, f footer) {
+	h()
+	b()
+	f()
+}
+
+func drawHeader() {
+	fmt.Println("=======================")
+	fmt.Println("| °C       | °F       |")
+	fmt.Println("=======================")
+}
+
+func drawFooter() {
+	fmt.Println("=======================")
+}
+
+func makeDrawBody(start celsius, end celsius, step celsius) body {
+	return func() {
+		for t:=start; t<=end; t+=step {
+			fmt.Printf("| %5.1f    | %5.1f    |\n", t, t.fahrenheit())
+		}
+	}
+}
+
 func main() {
-    for t:=-40; t<=100; t+=5 {
-        fmt.Printf("%v\n", t)
-    }
+	var drawBody body = makeDrawBody(celsius(-40), celsius(100), celsius(5))
+	drawTable(drawHeader, drawBody, drawFooter)
 }
