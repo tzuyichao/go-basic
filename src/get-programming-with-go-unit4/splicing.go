fbbcd6d931bab9dc6cd7c5beda1d6ceee449bbd6
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-20 13:59:39 +0800

    array splicing

---
+++ src/get-programming-with-go-unit4/splicing.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+    planets := [...]string {
+		"Mercury",
+		"Venus",
+		"Earth",
+		"Mars",
+		"Jupiter",
+		"Saturn",
+		"Uranus",
+		"Neptune",
+	}
+	terrestrial := planets[0:4]
+	gasGiants := planets[4:6]
+	iceGiants := planets[6:8]
+
+	fmt.Println(terrestrial, gasGiants, iceGiants)
+
+	iceGiants2 := iceGiants
+	iceGiants[1] = "Poseidon"
+	fmt.Println(planets)
+	fmt.Println(iceGiants)
+	fmt.Println(iceGiants2)
+}
