bb41940c64acdd39baa0a8b8b62fb0cec64bf606
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-21 17:14:40 +0800

    set

---
+++ src/get-programming-with-go-unit4/set.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+    var temperatures = []float64 {
+		-28.0, 32.0, -31.0, -29.0, -23.0, -29.0, -28.0, -33.0,
+	}
+	set := make(map[float64]bool)
+
+	fmt.Println(temperatures)
+
+	for _, t := range temperatures {
+		set[t] = true
+	}
+
+	if set[-28] {
+		fmt.Println("-28 is set member")
+	}
+
+	fmt.Println(set)
+
+	unique := make([]float64, 0, len(set))
+	for t := range set {
+		unique = append(unique, t)
+	}
+	sort.Float64s(unique)
+	fmt.Println(unique)
+}
