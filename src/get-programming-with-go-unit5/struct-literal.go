74729d0cf7885bf69e201399278b28e3428caa23
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-24 15:57:02 +0800

    struct literal

--- src/get-programming-with-go-unit5/struct-literal.go
+++ src/get-programming-with-go-unit5/struct-literal.go
@@ -12,6 +12,6 @@ func main() {
 	opportunity := location{lat: -1.9462, long: 354.4734}
 	fmt.Println(opportunity)
 
-	insight := location{lat: 4.5, long: 135.9}
+	insight := location{4.5, 135.9}
 	fmt.Println(insight)
 }
