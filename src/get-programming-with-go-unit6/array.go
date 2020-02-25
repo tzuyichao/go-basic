1a30a7ad2cd24fc4d668fdabde2c4261c71e7248
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-25 15:47:47 +0800

    mutating arrays

---
+++ src/get-programming-with-go-unit6/array.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+)
+
+func reset(board *[8][8]rune) {
+    board[0][0] = 'r'
+}
+
+func main() {
+    var board [8][8]rune
+
+    reset(&board)
+
+    fmt.Printf("%c\n", board[0][0])
+}
