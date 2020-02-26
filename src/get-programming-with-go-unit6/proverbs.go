d78afaf2e10e182911721f69dfe001c30329c25d
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-26 14:09:53 +0800

    handle error

---
+++ src/get-programming-with-go-unit6/proverbs.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func proverbs(name string) error {
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintln(f, "Errors are values.")
+	if err != nil {
+		f.Close()
+		return err
+	}
+	_, err = fmt.Fprintln(f, "Don't just check errors, handle them gracefully.")
+	f.Close()
+	return err
+}
+
+func main() {
+	err := proverbs("proverbs.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
