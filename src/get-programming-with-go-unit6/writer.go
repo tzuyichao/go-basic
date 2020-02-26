5cb798e3c1f223178cd21b9ce39e94d1bfe90bed
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-26 14:50:05 +0800

    safeWriter

---
+++ src/get-programming-with-go-unit6/writer.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "fmt"
+	"os"
+	"io"
+)
+
+type safeWriter struct {
+	w io.Writer
+	err error
+}
+
+func (sw *safeWriter) writeln(s string) {
+	if sw.err != nil {
+		return
+	}
+	_, sw.err = fmt.Fprintln(sw.w, s)
+}
+
+func proverbs(name string) error {
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	sw := safeWriter{w: f}
+    sw.writeln("Errors are values.")
+    sw.writeln("Don't just check errors, handle them gracefully.")
+    sw.writeln("Don't panic.")
+    sw.writeln("Make the zero value useful.")
+    sw.writeln("The bigger the interface, the weaker the abstraction.")
+    sw.writeln("interface{} says nothing.")
+    sw.writeln("Gofmt's style is no one's favorite, yet gofmt is everyone's favorite.")
+    sw.writeln("Documentation is for users.")
+    sw.writeln("A little copying is better than a little dependency.")
+    sw.writeln("Clear is better than clever.")
+    sw.writeln("Concurrency is not parallelism.")
+    sw.writeln("Don't communicate by sharing memory, share memory by communicating.")
+	sw.writeln("Channels orchestrate; mutexes serialize.")
+	return sw.err
+}
+
+func main() {
+    err := proverbs("proverbs.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
