9891cd93f3a8fe5130df385f17d3cbfe3ed8ebf9
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-27 09:59:56 +0800

    pipeline1

---
+++ src/get-programming-with-go-unit7/pipeline1/pipeline1.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func sourceGopher(downstream chan string) {
+	for _, v := range []string {"hello world", "a bad apple", "goodbye all"} {
+		downstream <- v
+	}
+	downstream <- ""
+}
+
+func filterGopher(upstream, downstream chan string) {
+	for {
+		item := <- upstream
+		if item == "" {
+			downstream <- ""
+			return
+		}
+		if !strings.Contains(item, "bad") {
+			downstream <- item
+		}
+	}
+}
+
+func printGopher(upstream chan string) {
+	for {
+		v := <- upstream
+		if v == "" {
+			return
+		}
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	c0 := make(chan string)
+	c1 := make(chan string)
+	go sourceGopher(c0)
+	go filterGopher(c0, c1)
+	printGopher(c1)
+}
