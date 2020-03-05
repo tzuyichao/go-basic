f193121d63713093b2c58497bfb844fb96f2463c
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-05 15:14:01 +0800

    trace example

---
+++ src/gopl.io/ch5/trace/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"log"
+)
+
+func bigSlowOperation() {
+	defer trace("bigSlowOperation")()
+	time.Sleep(10 * time.Second)
+}
+
+func trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() {
+		log.Printf("exit %s (%s)", msg, time.Since(start))
+	}
+}
+
+func main() {
+	fmt.Println("Starting main")
+	bigSlowOperation()
+	fmt.Println("Ending main")
+}
