4cdf5b0ff14f7622bf37d777930f37c803f7c170
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-06 09:04:15 +0800

    finish excercise

--- src/gopl.io/ch5/title1/main.go
+++ src/gopl.io/ch5/title1/main.go
@@ -3,6 +3,10 @@ package main
 import (
     "fmt"
 	"os"
+	"net/http"
+	"golang.org/x/net/html"
+	"strings"
+	"log"
 )
 
 func main() {
@@ -18,7 +22,39 @@ func main() {
 	}
 }
 
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
+
 func title(url string) error {
-	fmt.Println("Title: ttttttt")
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	contentType := resp.Header.Get("Content-Type")
+	log.Printf("Content-Type: %s\n", contentType)
+	if contentType != "text/html" && !strings.HasPrefix(contentType, "text/html") {
+		resp.Body.Close()
+		return fmt.Errorf("%s has type %s, not text/html", url, contentType)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("parse %s as HTML: %v", url, err)
+	}
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" && n.FirstChild != nil {
+			fmt.Println(n.FirstChild.Data)
+		}
+	}
+	forEachNode(doc, visitNode, nil)
 	return nil
 }
