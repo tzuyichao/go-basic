3fb5d80d26fd92f5aab6aa52cb0240a6c635729e
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-06 14:46:37 +0800

    parse html

---
+++ src/parse-html/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "log"
+    "fmt"
+	"golang.org/x/net/html"
+	"strings"
+)
+
+func main() {
+	s := `<p>Links:</p><ul><li><a href="foo">Foo</a><li><a href="/bar/baz">BarBaz</a></ul>`
+	doc, err := html.Parse(strings.NewReader(s))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	var f func(n *html.Node)
+
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					fmt.Println(a.Val)
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(doc)
+}
