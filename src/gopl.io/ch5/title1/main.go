c882ed7eb53e5962cffa685af64f2115ecf733c0
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-06 09:21:02 +0800

    finish

--- src/gopl.io/ch5/title1/main.go
+++ src/gopl.io/ch5/title1/main.go
@@ -22,18 +22,6 @@ func main() {
 	}
 }
 
-func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
-	if pre != nil {
-		pre(n)
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		forEachNode(c, pre, post)
-	}
-	if post != nil {
-		post(n)
-	}
-}
-
 func title(url string) error {
 	resp, err := http.Get(url)
 	if err != nil {
---
+++ src/gopl.io/ch5/title1/outline.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"golang.org/x/net/html"
+)
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
