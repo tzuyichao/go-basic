fc9172351288fb181a0be35c45726ebd32db9337
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-13 17:28:23 +0800

    book example

---
+++ src/bytecounter.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+// ByteCounter is for testing
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p))
+	return len(p), nil
+}
+
+func main() {
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c)
+
+	c = 0
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c)
+}
