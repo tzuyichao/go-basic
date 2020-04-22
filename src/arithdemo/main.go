5a3c01ae81863f5a254884e8594ff6aaf3e6e0b2
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-22 09:15:30 +0800

    book example

---
+++ src/arithdemo/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// from Go Programming by Example
+func main() {
+  var a, b int
+
+  a = 5
+  b = 10
+
+  c := a + b
+  fmt.Printf("%d + %d = %d\n", a, b, c)
+  d := a - b
+  fmt.Printf("%d - %d = %d\n", a, b, d)
+  e := float32(a) / float32(b)
+  fmt.Printf("%d / %d = %d\n", a, b, e)
+  f := a * b
+  fmt.Printf("%d * %d = %d\n", a, b, f)
+}
