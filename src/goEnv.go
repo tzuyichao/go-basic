709a6ce3bc8dc53d872ab7460633bf691d04deb5
tzuyichao <tzuyichao@mail2000.com.tw>
2021-04-19 13:44:02 +0800

    fmt

--- src/goEnv.go
+++ src/goEnv.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "runtime"
+	"fmt"
+	"runtime"
 )
 
 func main() {
 	fmt.Printf("You are using %v on %v machine\n", runtime.Compiler, runtime.GOARCH)
 	fmt.Printf("Using Go version %v\n", runtime.Version())
-	fmt.Printf("Number of CPUs: %v\n", runtime.NumCPU()) 
-    fmt.Printf("Number of Goroutines: %v\n", runtime.NumGoroutine()) 
+	fmt.Printf("Number of CPUs: %v\n", runtime.NumCPU())
+	fmt.Printf("Number of Goroutines: %v\n", runtime.NumGoroutine())
 }
