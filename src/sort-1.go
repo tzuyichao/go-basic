bd66eef4fdb566af68dbce3a43a6bb0c235858a3
tzuyichao <tzuyichao@mail2000.com.tw>
2021-04-20 09:13:04 +0800

    sort

---
+++ src/sort-1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	intData := []int{3, 1, 2, 5, 6, 4}
+	stringData := []string{"foo", "bar", "baz"}
+	floatData := []float64{1.5, 3.6, 2.5, 10.6}
+
+	sort.Ints(intData)
+	sort.Strings(stringData)
+	sort.Float64s(floatData)
+
+	fmt.Println("Sorted Integers: ", intData)
+	fmt.Println("Sorted Strings: ", stringData)
+	fmt.Println("Sorted Floats: ", floatData)
+}
