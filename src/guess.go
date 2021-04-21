7faa34ff7a767c237065fe385c8835b62912db01
tzuyichao <tzuyichao@mail2000.com.tw>
2021-04-21 16:50:41 +0800

    complete

---
+++ src/guess.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
+	//fmt.Println(target)
+
+	reader := bufio.NewReader(os.Stdin)
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
+		fmt.Print("Make a guess:")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("You guess:", guess)
+		if guess > target {
+			fmt.Println("Oops. Your guess was HIGH")
+		} else if guess < target {
+			fmt.Println("Oops. Your guess was LOW")
+		} else {
+			success = true
+			fmt.Println("You WIN!")
+			break
+		}
+	}
+	if !success {
+		fmt.Println("Sorry, you didn't guess my number. It was:", target)
+	}
+}
