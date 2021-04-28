c271ee7bdeb912214c63efd4dccf53ce123993f7
tzuyichao <tzuyichao@mail2000.com.tw>
2021-04-28 10:27:33 +0800

    read file

---
+++ src/readfile.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "bufio"
+    "log"
+    "os"
+)
+
+func main() {
+    file, err := os.Open("data.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        fmt.Println(scanner.Text())
+    }
+    err = file.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
+    if scanner.Err() != nil {
+        log.Fatal(scanner.Err())
+    }
+}
