3093ff61855d4e3a658f14aa396f0b00d28b926c
tzuyichao <tzuyichao@mail2000.com.tw>
2021-04-28 16:44:08 +0800

    slice lab 2

---
+++ src/slice_2.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    notes := make([]string, 7)
+    primes := make([]int, 5)
+
+    fmt.Println(len(notes))
+    fmt.Println(len(primes))
+}
