4c4d49458118890e15b13c28f7e102c9cd4a6e6d
tzuyichao <tzuyichao@mail2000.com.tw>
2021-05-06 15:13:14 +0800

    book example

---
+++ src/filepath_1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+)
+
+func main() {
+    if len(os.Args) != 2 {
+		fmt.Println("Please specify a path.")
+		return
+	}
+	root, err := filepath.Abs(os.Args[1])
+	if err != nil {
+		fmt.Println("Cannot get absolute path:", err)
+		return
+	}
+	fmt.Println("Listing files in", root)
+	var c struct {
+		files int
+		dirs int
+	}
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			c.dirs++
+		} else {
+			c.files++
+		}
+		fmt.Println("-", path)
+		return nil
+	})
+	fmt.Printf("Total: %d files in %d directories.\n", c.files, c.dirs)
+}
