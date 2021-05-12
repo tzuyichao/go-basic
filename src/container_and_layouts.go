d5db1f09921bf20194fff7b300ecb50ad77b63c9
tzuyichao <tzuyichao@mail2000.com.tw>
2021-05-12 11:14:46 +0800

    basic lab

---
+++ src/container_and_layouts.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "image/color"
+    "fyne.io/fyne/v2"
+    "fyne.io/fyne/v2/app"
+    "fyne.io/fyne/v2/canvas"
+    "fyne.io/fyne/v2/container"
+    "fyne.io/fyne/v2/layout"
+)
+
+func main() {
+    myApp := app.New()
+	myWindow := myApp.NewWindow("Container")
+
+	text1 := canvas.NewText("Hello", color.White)
+	text2 := canvas.NewText("There", color.White)
+	text2.Move(fyne.NewPos(20, 20))
+	text3 := canvas.NewText("World", color.White)
+	//content := container.NewWithoutLayout(text1, text2, text3)
+	content := container.New(layout.NewGridLayout(2), text1, text2, text3)
+
+	myWindow.SetContent(content)
+	myWindow.Resize(fyne.NewSize(100, 100))
+	myWindow.ShowAndRun()
+}
