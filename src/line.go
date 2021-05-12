11b2bb977f82139e619e28d5629df9f05efe5e6b
tzuyichao <tzuyichao@mail2000.com.tw>
2021-05-12 15:50:08 +0800

    canvas tour

---
+++ src/line.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"image/color"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+)
+
+func main() {
+	myApp := app.New()
+	w := myApp.NewWindow("Line")
+
+	line := canvas.NewLine(color.Black)
+	line.StrokeWidth = 5
+	w.SetContent(line)
+
+	w.Resize(fyne.NewSize(100, 100))
+	w.ShowAndRun()
+}
