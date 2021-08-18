d434de14982ca4f65e87a8d9b6389e525172b1a6
tzuyichao <tzuyichao@mail2000.com.tw>
2021-08-18 16:30:02 +0800

    partial

---
+++ src/workshopActivity12.01/go.mod
@@ -0,0 +1,3 @@
+module workshopActivity12.01
+
+go 1.16
---
+++ src/workshopActivity12.01/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("bank.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println(record)
+	}
+}
