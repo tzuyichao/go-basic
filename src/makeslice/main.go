a15a5c29ffb3e7ec94fb8d41141e7c881ca6070b
TzuYi Chao <tzuyichao0313@gmail.com>
2021-08-24 12:51:10 +0800

    append一定會增加slice長度

--- src/makeslice/main.go
+++ src/makeslice/main.go
@@ -11,4 +11,8 @@ func main() {
 	fmt.Println("data == nil:", data == nil)
 	var data2 = []int{}
 	fmt.Println("data2 == nil:", data2 == nil)
+	// append一定會增加slice的長度
+	y := make([]int, 5, 10)
+	y = append(y, 10)
+	fmt.Println(y, len(y), cap(y))
 }
