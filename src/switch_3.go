d2b2ce9bbc4f6b8eaa1f43122a350f6df64e7853
TzuYi Chao <tzuyichao0313@gmail.com>
2021-08-27 16:45:45 +0800

    switch and runtime Version()

---
+++ src/switch_3.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	words := []string{"hi", "salutations", "hello"}
+	for _, word := range(words) {
+		// empty switch
+		switch wordLen := len(word); {
+		case wordLen < 5:
+			fmt.Println(word, "is a short word!")
+		case wordLen > 10:
+			fmt.Println(word, "is a long word!")
+		default:
+			fmt.Println(word, "is exactly the right length");
+		}
+	}
+}
