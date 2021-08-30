0c81ab89ab056ee467f466a4ffae79c2ced89506
TzuYi Chao <tzuyichao0313@gmail.com>
2021-08-30 15:40:13 +0800

    selection sort

---
+++ src/selection_sort_1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func SelectionSort(src []int) []int {
+	var res []int = make([]int, len(src), cap(src))
+
+	copy(res, src)
+
+	for i := 0; i < len(res)-1; i++ {
+		minIdx := i
+		for j := minIdx+1; j < len(res); j++ {
+			if res[minIdx] > res[j] {
+				minIdx = j
+			}
+		}
+		if i != minIdx {
+			temp := res[i]
+			res[i] = res[minIdx]
+			res[minIdx] = temp
+		}
+	}
+
+	return res
+}
+
+func main() {
+	data := []int{4, 3, 2, 1}
+	res := SelectionSort(data)
+	fmt.Printf("%v\n", res)
+}
