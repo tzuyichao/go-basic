91abf33998ae6a57970a39d8cf830bb9be576fda
TzuYi Chao <tzuyichao0313@gmail.com>
2021-09-03 13:34:30 +0800

    promotion 1

---
+++ src/promotion_1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Employee struct {
+	Name string
+	ID string
+}
+
+func (e Employee) Description() string {
+	return fmt.Sprintf("%s (%s)", e.Name, e.ID)
+}
+
+type Manager struct {
+	Employee
+	Reports []Employee
+}
+
+func (m Manager) FindNewEmployees() []Employee {
+	fmt.Println("Called")
+}
+
+func main() {
+	m := Manager{
+		Employee: Employee{
+			Name: "Bob",
+			ID: "1234",
+		},
+		Reports: []Employee{},
+	}
+	fmt.Println(m)
+	fmt.Println(m.Description())
+}
