382716600acda5b860937170bd165a92cc4394a9
TzuYi Chao <tzuyichao0313@gmail.com>
2021-09-07 10:24:09 +0800

    errors is

---
+++ src/errors_is_1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+	"os"
+)
+
+func fileChecker(name string) error {
+	f, err := os.Open(name)
+	if err != nil {
+		return fmt.Errorf("in fileChecker: %w", err)
+	}
+	f.Close()
+	return nil
+}
+
+func main() {
+	err := fileChecker("not_here.txt")
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			fmt.Println("That file doesn't exist")
+		}
+	}
+}
