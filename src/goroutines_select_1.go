ee38b14fffc59a104fa3d7395b3a23278837faaf
TzuYi Chao <tzuyichao0313@gmail.com>
2021-09-09 15:01:11 +0800

    solve deadlock with select

---
+++ src/goroutines_select_1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go func() {
+		v := 1
+		ch1 <- v
+		v2 := <- ch2
+		log.Println("In goroutine: ", v, v2)
+	}()
+	v := 2
+	var v2 int
+	select {
+	case ch2 <- v:
+	case v2 = <- ch1:
+	}
+	log.Println("In main: ", v, v2)
+}
