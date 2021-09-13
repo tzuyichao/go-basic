dc5fae807a52c997bb702fd5444ac4902a936fc3
TzuYi Chao <tzuyichao0313@gmail.com>
2021-09-13 13:20:20 +0800

    sync.Map

---
+++ src/concurrent_map.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var names = []string{"Alan", "Joe", "Jack", "Ben", "Ellen", "Lisa", "Carl", "Steve", "Anton", "Yo"}
+
+func main() {
+	m := sync.Map{}
+	wg := &sync.WaitGroup{}
+	wg.Add(10)
+	for i:=0; i<10; i++ {
+		go func(idx int) {
+			m.Store(fmt.Sprintf("%d", idx), names[idx])
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	v, ok := m.Load("1")
+	if ok {
+		fmt.Printf("For Load key: 1 got %v\n", v)
+	}
+
+	v, ok = m.LoadOrStore("11", "Tonny")
+	if !ok {
+		fmt.Printf("key 11 missing stored val: %v\n", v)
+	}
+	m.Range(func(k, v interface{}) bool {
+		key, _ := k.(string)
+		name, _ := v.(string)
+		fmt.Printf("For index %v got %v\n", key, name)
+		return true
+	})
+}
