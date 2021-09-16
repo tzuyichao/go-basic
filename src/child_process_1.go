910de92555070849d81007dbabcbd51da789df72
TzuYi Chao <tzuyichao0313@gmail.com>
2021-09-16 15:09:08 +0800

    child  process info

---
+++ src/child_process_1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("cmd.exe", "dir")
+	if err := cmd.Start(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Cmd:", cmd.Args[0])
+	fmt.Println("Args:", cmd.Args[1:])
+	fmt.Println("PID:", cmd.Process.Pid)
+	cmd.Wait()
+}
