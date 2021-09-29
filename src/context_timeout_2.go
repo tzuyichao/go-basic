75855a2903104db2edf8f59eedeeca07aa9562c9
TzuYi Chao <tzuyichao0313@gmail.com>
2021-09-29 10:08:22 +0800

    context timeout

---
+++ src/context_timeout_2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"time"
+	"context"
+	"fmt"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	time.AfterFunc(time.Second*10, cancel)
+    done := ctx.Done()
+	for i:=0; ; i++ {
+		select {
+		case <-done:
+			fmt.Println("Exit: ", ctx.Err())
+			return
+		case <-time.After(time.Second):
+			fmt.Println("tick", i)
+		}
+	}
+}
