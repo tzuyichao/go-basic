ad7001f4f70c7351a7eb753bfb493ab0b78d70ac
TzuYi Chao <tzuyichao0313@gmail.com>
2021-10-07 09:52:33 +0800

    strings package lab 1

---
+++ src/strings_1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	s "strings"
+	"unicode"
+)
+
+var f = fmt.Printf
+
+func main() {
+	f("EqualFold: %v\n", s.EqualFold("Mihalis", "MIHAlis"))
+	f("EqualFold: %v\n", s.EqualFold("Mihalis", "MIHAli"))
+	f("Index: %v\n", s.Index("Mihalis", "ha"))
+	f("Index: %v\n", s.Index("Mihalis", "Ha"))
+	f("Prefix: %v\n", s.HasPrefix("Mihalis", "Mi"))
+	f("Prefix: %v\n", s.HasPrefix("Mihalis", "mi"))
+	f("Suffix: %v\n", s.HasSuffix("Mihalis", "is"))
+	f("Suffix: %v\n", s.HasSuffix("Mihalis", "IS"))
+	str := "This is  a\tstring\nwith space\rtest"
+	for _, ch := range str {
+		f("%v: isSpace: %v\n", ch, unicode.IsSpace(ch))
+	}
+	t := s.Fields(str)
+	f("Fields: %v\n", len(t))
+}
