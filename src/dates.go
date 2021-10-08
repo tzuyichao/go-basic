eb3c17bf50dfd5a1087a473e8aad798d0eceb477
TzuYi Chao <tzuyichao0313@gmail.com>
2021-10-08 10:26:00 +0800

    time package

---
+++ src/dates.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: dates parse_string")
+		return
+	}
+
+	dateString := os.Args[1]
+
+	d, err := time.Parse("02 January 2006", dateString)
+	if err == nil {
+		fmt.Println("Full:", d)
+		fmt.Println("Time:", d.Day(), d.Month(), d.Year())
+	}
+	d, err = time.Parse("02-01-2006 15:04", dateString)
+	if err == nil {
+		fmt.Println("Full:", d)
+		fmt.Println("Date:", d.Day(), d.Month(), d.Year())
+		fmt.Println("Time:", d.Hour(), d.Minute())
+	}
+	fmt.Println("start: ", start)
+}
