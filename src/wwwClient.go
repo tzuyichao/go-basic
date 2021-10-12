e4256e95c60aec78621b66618032c0de2390a9e2
TzuYi Chao <tzuyichao0313@gmail.com>
2021-10-12 15:23:17 +0800

    web client 1

---
+++ src/wwwClient.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"net/http"
+	"fmt"
+	"net/url"
+	"time"
+	"net/http/httputil"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Printf("Usage: %s URL\n", filepath.Base(os.Args[0]))
+		return
+	}
+	URL, err := url.Parse(os.Args[1])
+	if err != nil {
+		fmt.Println("Error in parsing:", err)
+		return
+	}
+	c := &http.Client{
+		Timeout: 15*time.Second,
+	}
+
+	request, err := http.NewRequest(http.MethodGet, URL.String(), nil)
+	if err != nil {
+		fmt.Println("Get: ", err)
+		return
+	}
+	httpData, err := c.Do(request)
+	if err != nil {
+		fmt.Println("Error in Do(): ", err)
+		return
+	}
+	defer httpData.Body.Close()
+	fmt.Println("Status Code:", httpData.Status)
+	header, _ := httputil.DumpResponse(httpData, false)
+	fmt.Println(string(header))
+	contentType := httpData.Header.Get("Content-Type")
+	characterSet := strings.SplitAfter(contentType, "charset=")
+	if len(characterSet) > 1 {
+		fmt.Println("Character Set:", characterSet[1])
+	}
+	if httpData.ContentLength == -1 {
+		fmt.Println("ContentLength is unknown!")
+	} else {
+		fmt.Println("ContentLength:", httpData.ContentLength)
+	}
+	length := 0
+	var buffer [1024]byte
+	r := httpData.Body
+	for {
+		n, err := r.Read(buffer[0:])
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		length = length + n
+	}
+	fmt.Println("Calculated response data length:", length)
+}
