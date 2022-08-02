13dd2c8df5bf5e75d216d632d7ea08d97e5788c1
TzuYi Chao <tzuyichao0313@gmail.com>
2022-08-02 11:05:59 +0800

    main.go without testing

---
+++ src/pragprog.com/rggo/interacting/todo/cmd/todo/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"pragprog.com/rggo/interacting/todo"
+)
+
+const todoFileName = ".todo.json"
+
+func main() {
+	l := &todo.List{}
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	switch {
+	case len(os.Args) == 1:
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+	default:
+		item := strings.Join(os.Args[1:], " ")
+		l.Add(item)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+}
