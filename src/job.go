9cdeda56658257bf73889b30736e392bc2463262
TzuYi Chao <tzuyichao0313@gmail.com>
2022-08-11 14:33:08 +0800

    channel again puzzle 18

---
+++ src/job.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Job struct {
+	State string
+	done chan struct{}
+}
+
+func (j *Job) Wait() {
+	<-j.done
+}
+
+func (j *Job) Done() {
+	j.State = "done"
+	close(j.done)
+}
+
+func main() {
+	ch := make(chan Job)
+	go func() {
+		j := <-ch
+		j.Done()
+	}()
+	job := Job{"ready", make(chan struct{})}
+	ch<-job
+	job.Wait()
+	fmt.Println(job.State)
+}
