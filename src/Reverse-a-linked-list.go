acb96b89734c645a5af770a8bee69947b9f6feed
TzuYi Chao <tzuyichao0313@gmail.com>
2022-09-11 09:16:35 +0800

    HackerRank Prepare Problem Solving 5

---
+++ src/Reverse-a-linked-list.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type SinglyLinkedListNode struct {
+	data int32
+	next *SinglyLinkedListNode
+}
+
+type SinglyLinkedList struct {
+	head *SinglyLinkedListNode
+	tail *SinglyLinkedListNode
+}
+
+func (singlyLinkedList *SinglyLinkedList) insertNodeIntoSinglyLinkedList(nodeData int32) {
+	node := &SinglyLinkedListNode{
+		next: nil,
+		data: nodeData,
+	}
+
+	if singlyLinkedList.head == nil {
+		singlyLinkedList.head = node
+	} else {
+		singlyLinkedList.tail.next = node
+	}
+
+	singlyLinkedList.tail = node
+}
+
+func printSinglyLinkedList(node *SinglyLinkedListNode, sep string, writer *bufio.Writer) {
+	for node != nil {
+		fmt.Fprintf(writer, "%d", node.data)
+
+		node = node.next
+
+		if node != nil {
+			fmt.Fprintf(writer, sep)
+		}
+	}
+}
+
+/*
+ * Complete the 'reverse' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     data int32
+ *     next *SinglyLinkedListNode
+ * }
+ *
+ */
+
+func reverse(llist *SinglyLinkedListNode) *SinglyLinkedListNode {
+	var curr *SinglyLinkedListNode
+	var res *SinglyLinkedListNode
+
+	curr = llist
+	var tmp []int32
+	for curr != nil {
+		tmp = append(tmp, curr.data)
+		curr = curr.next
+	}
+
+	res = &SinglyLinkedListNode{data: tmp[len(tmp)-1], next: nil}
+	curr = res
+	for i := len(tmp) - 2; i >= 0; i-- {
+		t := SinglyLinkedListNode{data: tmp[i], next: nil}
+		curr.next = &t
+		curr = &t
+	}
+
+	return res
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	testsTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	tests := int32(testsTemp)
+
+	for testsItr := 0; testsItr < int(tests); testsItr++ {
+		llistCount, err := strconv.ParseInt(readLine(reader), 10, 64)
+		checkError(err)
+
+		llist := SinglyLinkedList{}
+		for i := 0; i < int(llistCount); i++ {
+			llistItemTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+			checkError(err)
+			llistItem := int32(llistItemTemp)
+			llist.insertNodeIntoSinglyLinkedList(llistItem)
+		}
+
+		llist1 := reverse(llist.head)
+
+		printSinglyLinkedList(llist1, " ", writer)
+		fmt.Fprintf(writer, "\n")
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
