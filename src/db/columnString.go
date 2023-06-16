dd0bdf1d8f8343ca0d7c96620978a2db45738bdf
TzuYi Chao <tzuyichao0313@gmail.com>
2023-06-16 10:33:45 +0800

    test

---
+++ src/db/columnString.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+func main() {
+	if len(os.Args) < 6 {
+		fmt.Println("Usage: go run os.Args[0] [host] [user] [password] [database] [tableName]")
+		os.Exit(1)
+	}
+	host := os.Args[1]
+	user := os.Args[2]
+	password := os.Args[3]
+	database := os.Args[4]
+	tableName := os.Args[5]
+
+	connString := fmt.Sprintf("sqlserver://%s:%s@%s?database=%s&connection+timeout=30", user, password, host, database)
+	db, err := sql.Open("sqlserver", connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Connect to Database failed:", err.Error())
+		os.Exit(1)
+	}
+
+	query := fmt.Sprintf("SELECT TOP 1 * FROM %s", tableName)
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	columnNames, err := rows.Columns()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := strings.Join(columnNames, ",")
+	fmt.Println(result)
+}
