5af8a684fcae65dccb64becd45b4c190f1d9ed22
TzuYi Chao <tzuyichao0313@gmail.com>
2023-06-16 09:26:47 +0800

    fix

--- src/zookeeper/health/health.go
+++ src/zookeeper/health/health.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/samuel/go-zookeeper/zk"
 )
 
 func main() {
-	zkAddresses := []string{"localhost:2181"}
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s <zk-address>\n", os.Args[0])
+		os.Exit(1)
+	}
+	zkAddresses := []string{os.Args[1]}
 	conn, _, err := zk.Connect(zkAddresses, time.Second)
 	if err != nil {
 		log.Fatal(err)
